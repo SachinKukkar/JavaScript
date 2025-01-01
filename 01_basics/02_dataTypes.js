42ed3dd98284fc37ccd92f58798fabcdfc274da0
Sachin Kukkar <119757438+SachinKukkar@users.noreply.github.com>
2025-01-01 16:12:12 +0000

    tutorial 5 done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict" ;  // treat all JS code as newer version 
+
+// alert("Hello world") // We are using NodeJs not browser
+
+// Our main aim is to make the "code readability HIGH"
+
+// mdn documentation and tc39.es website for reading standards of javascript
+
+console.log("Sachin")
+
+let name = "Sachin"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number ==> 2 to the power 53
+// bigint ==> bohot bada number jaise trading site , facebook etc.
+// string ==> " " double qoutes are preferrable
+// boolean ==> true/false 
+
+// null ==> standalone value 
+// undefined ==> value defined nahi hai matlab variable me value assign nhi huyi hai --> temperature wala example
+
+// symbol ==> unique
+
+/* Object -> most important */
+
+console.log(typeof "Sachin")
+console.log(typeof age)
+
+console.log(typeof null)  // iska output 'object' milega
+console.log(typeof undefined)  // iska output 'undefined' milega
