956070732296f50789358c4d202df14ece46f070
Sachin Kukkar <119757438+SachinKukkar@users.noreply.github.com>
2025-01-02 12:17:46 +0000

    tutorial 8 done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);   // predictable result nhi hai-> value conversion ka problem ajata hai
+
+
+// tripe equals to --> === it checks and compares the values strictly , mtlb value k sath sath data type b check hoga
+
+console.log("2"===2);
