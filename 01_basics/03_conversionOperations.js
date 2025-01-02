5005a810dd9abde3fbcb50e08a7bad81816f6a5d
Sachin Kukkar <119757438+SachinKukkar@users.noreply.github.com>
2025-01-02 12:10:09 +0000

    tutorial 7 done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,4 +1,4 @@
-let score = "33abc"
+/* let score = "33abc"
 
 console.log(typeof score)
 console.log(typeof(score))
@@ -24,3 +24,34 @@ let stringNumber = String(someNumber)
 
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+*/
+
+
+
+// ************************* Operations ***************************// 
+
+let value = 3 
+let negValue = -value
+console.log(negValue);
+
+/*
+console.log(2+3)
+console.log(2-3)
+console.log(2*3)
+console.log(2**3)
+console.log(2/3)
+console.log(2%3)
+
+*/
+
+// let str1 = "Hello,"
+// let str2 = " Sachin"
+// let str3 = str1+str2
+
+// console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
